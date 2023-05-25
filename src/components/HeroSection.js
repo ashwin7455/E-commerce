426acc02187d247a93b3b82b7b3631d6658d31b9
ashwin7455 <ashwinyaadav@gmail.com>
2023-05-26 04:55:03 +0530

    error solved

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import { Button } from '../styles/Button';
 
 
 const HeroSection = ({myData}) => {
-   const {name} = myData;
+   
     return (
         <Wrapper>
             <div className='container'>
@@ -15,7 +15,7 @@ const HeroSection = ({myData}) => {
                         <p className='intro-data'>
                             WELCOME TO
                         </p>
-                        <h1>{name}</h1>
+                        <h1>Yadav</h1>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
                         </p>
                         <NavLink to="/product">
