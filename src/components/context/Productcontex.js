58c1a437ab723f86ac8b8fed42e52722308030c4
ashwin7455 <ashwinyaadav@gmail.com>
2023-05-28 23:45:05 +0530

    fetching api

--- src/components/context/Productcontex.js
+++ src/components/context/Productcontex.js
@@ -1,8 +1,25 @@
-import { createContext, useContext } from "react";
+import axios from "axios";
+import { createContext, useContext, useEffect } from "react";
 
 const AppContext = createContext()
+ 
+const API = "https://api.pujakaitem.com/api/products"
 
 const AppProvider = ({children}) => {
+
+ const getProducts=  async(url) => {
+   const res = await axios.get(url)
+   const products = await res.data;
+   console.log(products)
+ }
+
+
+
+  useEffect(() => {
+  getProducts(API);
+},[])
+
+
     return <AppContext.Provider value ={{ myName: "ashwin" }}>
         {children}
         </AppContext.Provider>
@@ -14,4 +31,4 @@ const useProductContext = () => {
 }
 
 
-export {AppProvider , AppContext , useProductContext};
+export {AppProvider , AppContext , useProductContext    };
