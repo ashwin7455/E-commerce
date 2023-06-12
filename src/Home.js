39db2eadbe121cb59bec2f2e53be2b006456c08a
ashwin7455 <ashwinyaadav@gmail.com>
2023-06-13 02:04:47 +0530

    product page added

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import HeroSection from "./components/HeroSection";
 import Services from "./components/Services";
 import Trusted from "./components/Trusted";
+import FeatureProduct from "./components/FeatureProduct";
 
 
 
@@ -14,6 +15,7 @@ const Home = () => {
     <>
       <Wrapper>
         <HeroSection  myData = {data}/>
+        <FeatureProduct/>
         <Services/>
         <Trusted/>
         
